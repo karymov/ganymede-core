5bf5bf19971f4d021886178e09e21b116e0ef331
vladimir zaytsev <vladimir.zaycev.91@gmail.com>
2010-08-10 01:05:11 +0600

    db structure update

--- sql_dump/postgresql/public.sql
+++ sql_dump/postgresql/public.sql
@@ -1,25 +1,57 @@
+/*
+ Navicat Premium Data Transfer
+
+ Source Server         : ganymede local
+ Source Server Type    : PostgreSQL
+ Source Server Version : 80404
+ Source Host           : localhost
+ Source Database       : ganymede
+ Source Schema         : public
+
+ Target Server Type    : PostgreSQL
+ Target Server Version : 80404
+ File Encoding         : utf-8
+
+ Date: 08/10/2010 01:04:44 AM
+*/
+
+-- ----------------------------
+--  Sequence structure for "accounts_id_seq"
+-- ----------------------------
 DROP SEQUENCE IF EXISTS "accounts_id_seq";
-CREATE SEQUENCE "accounts_id_seq" INCREMENT 1 START 18 MAXVALUE 9223372036854775807 MINVALUE 1 CACHE 1;
+CREATE SEQUENCE "accounts_id_seq" INCREMENT 1 START 22 MAXVALUE 9223372036854775807 MINVALUE 1 CACHE 1;
 ALTER TABLE "accounts_id_seq" OWNER TO "postgres";
-DROP SEQUENCE IF EXISTS "data_nodes_id_seq";
-CREATE SEQUENCE "data_nodes_id_seq" INCREMENT 1 START 18 MAXVALUE 9223372036854775807 MINVALUE 1 CACHE 1;
-ALTER TABLE "data_nodes_id_seq" OWNER TO "postgres";
+
+-- ----------------------------
+--  Sequence structure for "book_metas_id_seq"
+-- ----------------------------
 DROP SEQUENCE IF EXISTS "book_metas_id_seq";
 CREATE SEQUENCE "book_metas_id_seq" INCREMENT 1 START 18 MAXVALUE 9223372036854775807 MINVALUE 1 CACHE 1;
 ALTER TABLE "book_metas_id_seq" OWNER TO "postgres";
+
+-- ----------------------------
+--  Sequence structure for "category_metas_id_seq"
+-- ----------------------------
 DROP SEQUENCE IF EXISTS "category_metas_id_seq";
 CREATE SEQUENCE "category_metas_id_seq" INCREMENT 1 START 18 MAXVALUE 9223372036854775807 MINVALUE 1 CACHE 1;
 ALTER TABLE "category_metas_id_seq" OWNER TO "postgres";
+
+-- ----------------------------
+--  Sequence structure for "data_nodes_id_seq"
+-- ----------------------------
+DROP SEQUENCE IF EXISTS "data_nodes_id_seq";
+CREATE SEQUENCE "data_nodes_id_seq" INCREMENT 1 START 526151 MAXVALUE 9223372036854775807 MINVALUE 1 CACHE 1;
+ALTER TABLE "data_nodes_id_seq" OWNER TO "postgres";
 
 -- ----------------------------
 --  Table structure for "accounts"
 -- ----------------------------
 DROP TABLE IF EXISTS "accounts";
 CREATE TABLE "accounts" (
-    "id" int8 NOT NULL DEFAULT nextval('accounts_id_seq'::regclass),
-    "login" varchar(32) DEFAULT NULL::character varying,
+	"id" int8 NOT NULL DEFAULT nextval('accounts_id_seq'::regclass),
+	"login" varchar(32) DEFAULT NULL::character varying,
 	"person_id" varchar(32) DEFAULT NULL::character varying,
-	"password" varchar(32) DEFAULT NULL::character varying,
+	"password" varchar(64) DEFAULT NULL::character varying,
 	"name" varchar(32) DEFAULT NULL::character varying,
 	"surname" varchar(32) DEFAULT NULL::character varying,
 	"email" varchar(32) DEFAULT NULL::character varying,
@@ -33,54 +65,6 @@ WITH (OIDS=FALSE);
 ALTER TABLE "accounts" OWNER TO "postgres";
 
 -- ----------------------------
---  Table structure for "data_nodes"
--- ----------------------------
-DROP TABLE IF EXISTS "data_nodes";
-CREATE TABLE "data_nodes" (
-    "id" int8 NOT NULL DEFAULT nextval('data_nodes_id_seq'::regclass),
-	"name" varchar(32) DEFAULT NULL,
-	"type" int2 DEFAULT 0,
-	"parent" int8 DEFAULT NULL,
-	"children" varchar DEFAULT NULL
-)
-WITH (OIDS=FALSE);
-ALTER TABLE "data_nodes" OWNER TO "postgres";
-
--- ----------------------------
---  Table structure for "book_metas"
--- ----------------------------
-DROP TABLE IF EXISTS "book_metas";
-CREATE TABLE "book_metas" (
-    "id" int8 NOT NULL DEFAULT nextval('book_metas_id_seq'::regclass),
-	"name" varchar DEFAULT NULL,
-	"author_id" int8 DEFAULT NULL,
-	"publisher_id" int8 DEFAULT NULL,
-	"year" int2 DEFAULT NULL,
-	"pages_count" int2 DEFAULT NULL,
-	"abstract" varchar DEFAULT NULL,
-	"discipline" varchar DEFAULT NULL,
-	"resources_ids" varchar DEFAULT NULL,
-	"covers_ids" varchar DEFAULT NULL
-)
-WITH (OIDS=FALSE);
-ALTER TABLE "book_metas" OWNER TO "postgres";
-
--- ----------------------------
---  Table structure for "category_metas"
--- ----------------------------
-DROP TABLE IF EXISTS "category_metas";
-CREATE TABLE "category_metas" (
-    "id" int8 NOT NULL DEFAULT nextval('category_metas_id_seq'::regclass),
-	"name" varchar DEFAULT NULL,
-	"discipline" varchar DEFAULT NULL,
-	"description" varchar DEFAULT NULL,
-	"resources_ids" varchar DEFAULT NULL,
-	"covers_ids" varchar DEFAULT NULL
-)
-WITH (OIDS=FALSE);
-ALTER TABLE "category_metas" OWNER TO "postgres";
-
--- ----------------------------
 --  Table structure for "person_metas"
 -- ----------------------------
 DROP TABLE IF EXISTS "person_metas";
@@ -91,6 +75,49 @@ CREATE TABLE "person_metas" (
 )
 WITH (OIDS=FALSE);
 ALTER TABLE "person_metas" OWNER TO "postgres";
+
+-- ----------------------------
+--  Table structure for "book_metas"
+-- ----------------------------
+DROP TABLE IF EXISTS "book_metas";
+CREATE TABLE "book_metas" (
+	"id" int8 NOT NULL DEFAULT nextval('book_metas_id_seq'::regclass),
+	"name" varchar(64) DEFAULT NULL,
+	"author_id" int8 DEFAULT NULL,
+	"publisher_id" int8 DEFAULT NULL,
+	"year" int2 DEFAULT NULL,
+	"pages_count" int2 DEFAULT NULL,
+	"abstract" varchar DEFAULT NULL,
+	"discipline" varchar DEFAULT NULL
+)
+WITH (OIDS=FALSE);
+ALTER TABLE "book_metas" OWNER TO "postgres";
+
+-- ----------------------------
+--  Table structure for "category_metas"
+-- ----------------------------
+DROP TABLE IF EXISTS "category_metas";
+CREATE TABLE "category_metas" (
+	"id" int8 NOT NULL DEFAULT nextval('category_metas_id_seq'::regclass),
+	"name" varchar(64) DEFAULT NULL,
+	"discipline" varchar DEFAULT NULL,
+	"description" varchar DEFAULT NULL
+)
+WITH (OIDS=FALSE);
+ALTER TABLE "category_metas" OWNER TO "postgres";
+
+-- ----------------------------
+--  Table structure for "data_nodes"
+-- ----------------------------
+DROP TABLE IF EXISTS "data_nodes";
+CREATE TABLE "data_nodes" (
+	"id" int8 NOT NULL DEFAULT nextval('data_nodes_id_seq'::regclass),
+	"name" varchar(64) DEFAULT NULL::character varying,
+	"type" int2 DEFAULT 0,
+	"parent" int8 DEFAULT NULL
+)
+WITH (OIDS=FALSE);
+ALTER TABLE "data_nodes" OWNER TO "postgres";
 
 -- ----------------------------
 --  Table structure for "resource_metas"
@@ -128,9 +155,9 @@ ALTER SEQUENCE "accounts_id_seq" OWNED BY "accounts"."id";
 ALTER TABLE "accounts" ADD CONSTRAINT "accounts_pkey" PRIMARY KEY ("id");
 
 -- ----------------------------
---  Primary key structure for table "data_nodes"
+--  Primary key structure for table "person_metas"
 -- ----------------------------
-ALTER TABLE "data_nodes" ADD CONSTRAINT "data_nodes_pkey" PRIMARY KEY ("id");
+ALTER TABLE "person_metas" ADD CONSTRAINT "person_metas_pkey" PRIMARY KEY ("id");
 
 -- ----------------------------
 --  Primary key structure for table "book_metas"
@@ -143,7 +170,7 @@ ALTER TABLE "book_metas" ADD CONSTRAINT "book_metas_pkey" PRIMARY KEY ("id");
 ALTER TABLE "category_metas" ADD CONSTRAINT "category_metas_pkey" PRIMARY KEY ("id");
 
 -- ----------------------------
---  Primary key structure for table "person_metas"
+--  Primary key structure for table "data_nodes"
 -- ----------------------------
-ALTER TABLE "person_metas" ADD CONSTRAINT "person_metas_pkey" PRIMARY KEY ("id");
+ALTER TABLE "data_nodes" ADD CONSTRAINT "data_nodes_pkey" PRIMARY KEY ("id");
 
